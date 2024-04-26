a8a6acc805e4c7e0f21b25ac8835041a41ab6711
Pablobf19 <juampa2045@gmail.com>
2024-04-25 19:39:22 -0500

    metodo empleado

--- Proyecto.java
+++ Proyecto.java
@@ -1,6 +1,33 @@
-public class Proyecto {
+import java.util.LinkedList;
+import java.util.Scanner;
 
+public class Proyecto {
+    
     public static void main(String[] args) {
         
+        LinkedList<Empleado> empleados = new LinkedList<>();
+        Scanner sc = new Scanner(System.in);
+
+        for (int i = 0; i < 3; i++) {
+            Empleado item = new Empleado();
+            System.out.println("Ingrese el nombre del empleado: ");
+            item.setNombre(sc.next());
+            System.out.println("Ingrese el apellido del empleado: ");
+            item.setApellido(sc.next());
+            System.out.println("Ingrese la direccion del empleado: ");
+            item.setDireccion(sc.next());
+            System.out.println("Ingrese la edad del empleado: ");
+            item.setEdad(sc.nextInt());    
+            System.out.println("Ingrese el cargo del empleado: ");
+            item.setCargo(sc.next());
+            empleados.add(item);
+        }
+        String cadena ="";
+
+        for (Empleado empleado : empleados) {
+            cadena = cadena + empleado.getNombre() + "\n" + empleado.getApellido() + "\n" + empleado.getDireccion() + "\n" +
+             empleado.getEdad() + "\n" + empleado.getCargo();
+            System.out.println(cadena);
+        }
     }
 }
